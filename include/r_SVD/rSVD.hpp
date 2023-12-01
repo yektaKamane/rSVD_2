740996f163ae06b3c620e51d37b6a993c2ab8e4a
r499y <nicoraga36@gmail.com>
2023-12-01 12:29:45 +0000

    fixed with eigen

--- include/r_SVD/rSVD.hpp
+++ include/r_SVD/rSVD.hpp
@@ -1,8 +1,14 @@
+
 #ifndef rSVD_H
 #define rSVD_H
 
+#include "../powerMethod/PM.hpp"
+#include "../QRdecomposition/QR.hpp"
+#include <Eigen/Dense>
 
- void rSVD(const Matrix& A, Matrix& U, Vector& S, Matrix& V);
+using mat=Eigen::MatrixXd;
+using vet=Eigen::VectorXd;
+ void rSVD(mat& A, mat&U, vet& S, mat& V);
 
 
 
