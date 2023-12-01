5ab004f1cc653a93002669bbdb5b249eabfacc58
r499y <nicoraga36@gmail.com>
2023-12-01 10:17:24 +0000

    update with eigen

--- src/rSVD.cpp
+++ src/rSVD.cpp
@@ -1,36 +1,34 @@
 #include "../include/r_SVD/rSVD.hpp"
 
-#include "../include/powerMethod/PM2_full.hpp"
-#include "../include/dataStructure/matrix.hpp"
-#include "../include/dataStructure/vector.hpp"
+#include "../include/powerMethod/PM.hpp"
 #include "../include/QRdecomposition/QR.hpp"
 
 
- void rSVD(const Matrix& A, Matrix& U, Vector& S, Matrix& V) {
+ void rSVD(mat& A, mat& U, vet& S, mat& V) {
     // Stage A
     // (1) Form an n × (k + p) Gaussian random matrix G.
-    int n=A.getRows();
-    int m=A.getCols();
+    int n=A.rows();
+    int m=A.cols();
     //Matrix G(n,m);
     // code to generate Gaussian random matrix G
-    Matrix G=gaussianRandomMatrix(n,m);
+    mat G = mat::Random(n, m);
     // (2) Form the sample matrix Y = A G.
-    Matrix Y = A.multiply(G);
+    mat Y = A * G;
 
     // (3) Orthonormalize the columns of the sample matrix Q = orth(Y).
-    std::tuple<Matrix, Matrix> qr_result = qr_decomposition(Y);
-    Matrix Q= std::get<0>(qr_result);
+    std::tuple<mat, mat> qr_result = qr_decomposition(Y);
+    mat Q = std::get<0>(qr_result);
 
     // Stage B
     // (4) Form the (k + p) × n matrix B = Q∗A.
-    Matrix B = (Q.transpose()).multiply(A);
+    mat B = Q.transpose() * A;
 
     // (5) Form the SVD of the small matrix B: B = UDV ˆ
-    Matrix U_hat;
+    mat U_hat;
     PM(B, U_hat, S, V);
 
     // (6) Form U = QUˆ
-    U = Q.multiply(U_hat);
+    U = Q * U_hat;
     // END
 }
     
