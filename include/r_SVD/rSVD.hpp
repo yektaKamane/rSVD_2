ffe5b68cb2eb564e396f3676057b718922e02df0
yektaKamane <ykamaneh@gmail.com>
2023-12-03 22:42:08 +0000

    rSVD incompatibility

--- include/r_SVD/rSVD.hpp
+++ include/r_SVD/rSVD.hpp
@@ -1,6 +1,12 @@
 #ifndef rSVD_H
 #define rSVD_H
 
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+
+
+using mat = Eigen::MatrixXd;
+using vet = Eigen::VectorXd;
 
 void rSVD(mat& A, mat& U, vet& S, mat& V);
 
