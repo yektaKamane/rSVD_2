8dcf85cdace0212a98aeba7519cb0e2572eeb6c4
yektaKamane <ykamaneh@gmail.com>
2024-01-03 23:00:19 +0000

    mp update

--- mp.cpp
+++ mp.cpp
@@ -16,14 +16,14 @@ int main(int argc, char** argv) {
     srand(time(NULL));
 
     // Define the size of the matrix
-    const int size = 100;
+    const int size = 4;
 
     // Define a dynamic matrix A of size 10x10 and initialize with random values
     Eigen::MatrixXd A = Eigen::MatrixXd::Random(size, size);
-    // A << 1, 2, 3, 4,
-    //     2, 2, 2, 3,
-    //     1, 5, 5, 1,
-    //     8, 8, 1, 0;
+    A << 1, 2, 3, 4,
+        2, 2, 2, 3,
+        1, 5, 5, 1,
+        8, 8, 1, 0;
 
     // Define a dynamic vector b of size 10 and initialize with ones
     Eigen::VectorXd b = Eigen::VectorXd::Ones(size);
@@ -32,33 +32,42 @@ int main(int argc, char** argv) {
 
     // Divide rows of A among processors
     int rows_per_proc = A.rows() / num_procs;
-    Eigen::MatrixXd local_A = A.block(rank * rows_per_proc, 0, rows_per_proc, A.cols());
+    int rows_remainder = A.rows() % num_procs;
+    int local_rows = (rank < rows_remainder) ? rows_per_proc + 1 : rows_per_proc;
+    int offset = rank * rows_per_proc + std::min(rank, rows_remainder);
+
+    Eigen::MatrixXd local_A = A.block(offset, 0, local_rows, A.cols());
 
     // Broadcast vector b to all processors
     MPI_Bcast(b.data(), b.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     // Compute local portion of the result vector c manually
-    Eigen::VectorXd local_c(rows_per_proc);
+    Eigen::VectorXd local_c(local_rows);
 
-    auto start_time = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < rows_per_proc; ++i) {
+    for (int i = 0; i < local_rows; ++i) {
         local_c(i) = 0.0;
         for (int j = 0; j < A.cols(); ++j) {
             local_c(i) += local_A(i, j) * b(j);
         }
     }
-    
-    MPI_Gather(local_c.data(), rows_per_proc, MPI_DOUBLE, c.data(), rows_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    auto end_time = std::chrono::high_resolution_clock::now();
-    
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    // Gather local results using MPI_Gatherv
+    // Compute displacements and recvcounts arrays
+    std::vector<int> recvcounts(num_procs);
+    std::vector<int> displacements(num_procs);
+
+    for (int i = 0; i < num_procs; ++i) {
+        recvcounts[i] = (i < rows_remainder) ? rows_per_proc + 1 : rows_per_proc;
+        displacements[i] = i * rows_per_proc + std::min(i, rows_remainder);
+    }
+
+    MPI_Gatherv(local_c.data(), local_rows, MPI_DOUBLE, c.data(), recvcounts.data(), displacements.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     // Display the matrix A, vector b, and vector c
     if (rank == 0) {
-        // std::cout << "Matrix A:\n" << A << "\n\n";
-        // std::cout << "Vector b:\n" << b << "\n\n";
-        // std::cout << "Vector c:\n" << c << "\n\n";
-        std::cout << "Execution Time: " << duration.count() << " microseconds\n";
+        std::cout << "Matrix A:\n" << A << "\n\n";
+        std::cout << "Vector b:\n" << b << "\n\n";
+        std::cout << "Vector c:\n" << c << "\n\n";
     }
 
     // Finalize MPI
